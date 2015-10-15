2f288aa3cdb2588ba10f827cd068f17f8902f400
rchen45 <rchen@urhwirelessprvnat-172-18-68-145.near.illinois.edu>
2015-10-15 00:06:14 -0500

    Authentication for Users

---
+++ app/users/authentication.py
@@ -0,0 +1,21 @@
+from functools import wraps
+frmo flask import request, Response
+
+def check_auth(username, password):
+ // Check if a username /password combination is valid
+ 
+	return username == 'admin' and password == 'secret'
+	
+def authenticate():
+	return Response('Could not verify your access.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    
+def requires_auth(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		auth = request.authorization
+		if not auth or not check_auth(auth.username, auth.password):
+			return authenticate()
+		return f(*args, **kwargs)
+	return decorated
