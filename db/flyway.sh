9a260117ac54f5bf67f139574c6d30b0799091fb
Nick Magerko <nmagerko@gmail.com>
2015-10-28 22:49:49 -0500

    Got database migrations working with staging server

--- db/flyway.sh
+++ db/flyway.sh
@@ -2,14 +2,14 @@ LOCAL_USER=${MYSQL_USERNAME:='root'}
 LOCAL_PASSWORD=${MYSQL_PASSWORD:=''}
 LOCAL_HOST=${MYSQL_HOST:='localhost'}
 LOCAL_PORT=${MYSQL_PORT:='3306'}
-LOCAL_SCHEMA='conversationIM'
 
-STAGING_USER=$OPENSHIFT_MYSQL_DB_USERNAME
-STAGING_PASSWORD=$OPENSHIFT_MYSQL_DB_PASSWORD
-STAGING_SCHEMA='staging'
+STAGING_USER=$STAGING_MYSQL_USERNAME
+STAGING_PASSWORD=$STAGING_MYSQL_PASSWORD
 
-LOCAL_CONNECTION="jdbc:mysql://${LOCAL_HOST}:${LOCAL_PORT}/${LOCAL_SCHEMA}"
-STAGING_CONNECTION="jdbc:${OPENSHIFT_MYSQL_DB_URL}${STAGING_SCHEMA}"
+SCHEMA='conversationIM'
+
+LOCAL_CONNECTION="jdbc:mysql://${LOCAL_HOST}:${LOCAL_PORT}/${SCHEMA}"
+STAGING_CONNECTION="jdbc:mysql://localhost:3306/${SCHEMA}"
 
 SCRIPT_DIR=$(dirname $0)
 
@@ -23,11 +23,14 @@ fi
 if [ "$2" = "local" ]; then
 	flyway -user=$LOCAL_USER -password=$LOCAL_PASSWORD -url=$LOCAL_CONNECTION -locations=filesystem:$SCRIPT_DIR/migration -baselineOnMigrate=true -sqlMigrationSuffix=.sql $1
 elif [ "$2" = "staging" ]; then
-    if [ -z $STAGING_USER]; then
-      echo "The requested target ($2) is not available"
-      exit 2
-    fi
-	flyway -user=$STAGING_USER -password=$STAGING_PASSWORD -url=$STAGING_CONNECTION -locations=filesystem:$SCRIPT_DIR/migration -baselineOnMigrate=true -sqlMigrationSuffix=.sql $1
+  read -r -p "Is port-forwarding enabled? [y/N] " response
+  case $response in
+    [yY][eE][sS]|[yY]) 
+        flyway -user=$STAGING_USER -password=$STAGING_PASSWORD -url=$STAGING_CONNECTION -locations=filesystem:$SCRIPT_DIR/migration -baselineOnMigrate=true -sqlMigrationSuffix=.sql $1
+        ;;
+    *)
+        echo "Run 'rhc port-forward staging'"
+  esac
 else
 	echo "The requested target ($2) is not a valid"
 fi
